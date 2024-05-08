cc21cf39d5b5e4d3901fc96eb12157cbfa085833
Jeffrey Seneff <jeffsnff@gmail.com>
2024-05-08 16:23:11 -0600

    Added route to get all contacts

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express()
 const PORT = 3001;
+app.use(express.json());
 
+const contacts = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
 
-
+app.get('/contacts', (request, response) => {
+  response.json(contacts);
+})
 
 app.listen(PORT);
 console.log(`Server is running on ${PORT}`);
